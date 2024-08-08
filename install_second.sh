c27c9d66ba981316de518fed6262fd25fd02a740
fulken <32429584+fulken@users.noreply.github.com>
2024-08-08 20:29:05 +0330

    Update install_second.sh

--- install_second.sh
+++ install_second.sh
@@ -9,26 +9,17 @@ cyan='\033[0;36m'
 blue='\033[0;34m'
 rest='\033[0m'
 
-# If running in Termux, update and upgrade
-if [ -d "$HOME/.termux" ] && [ -z "$(command -v jq)" ]; then
-    echo "Running update & upgrade ..."
-    pkg update -y
-    pkg upgrade -y
-fi
-
-# Function to install necessary packages
+# Install necessary packages (if needed)
 install_packages() {
     local packages=(curl jq bc)
     local missing_packages=()
 
-    # Check for missing packages
     for pkg in "${packages[@]}"; do
         if ! command -v "$pkg" &> /dev/null; then
             missing_packages+=("$pkg")
         fi
     done
 
-    # If any package is missing, install missing packages
     if [ ${#missing_packages[@]} -gt 0 ]; then
         if [ -n "$(command -v pkg)" ]; then
             pkg install "${missing_packages[@]}" -y
@@ -47,25 +38,20 @@ install_packages() {
     fi
 }
 
-# Install the necessary packages
 install_packages
 
 # Clear the screen
 clear
 
-# Prompt for Authorization
+# Prompt for Authorization and other inputs
 echo -e "${purple}=======${yellow}Hamster Combat Auto Buy best cards${purple}=======${rest}"
 echo ""
 echo -en "${green}Enter Authorization [${cyan}Example: ${yellow}Bearer 171852....${green}]: ${rest}"
 read -r Authorization
 echo -e "${purple}============================${rest}"
-
-# Prompt for minimum balance threshold
 echo -en "${green}Enter minimum balance threshold (${yellow}the script will stop purchasing if the balance is below this amount${green}):${rest} "
 read -r min_balance_threshold
 echo -e "${purple}============================${rest}"
-
-# Prompt for second card threshold
 echo -en "${green}Enter the Threshold that you want to buy a new card? (${yellow}number below 1 to always buy the best card and greater then 1 to buying the second best card threshold${green}):${rest}"
 read -r threshold
 
@@ -129,7 +115,7 @@ wait_for_cooldown() {
     done
 }
 
-# Function to choose between the best and second best items considering cooldown
+# Function to choose between two sets of values
 choose_card_to_upgrade() {
     local best_item_id=$1
     local section=$2 
@@ -140,10 +126,17 @@ choose_card_to_upgrade() {
     local next_item_section=$7 
     local next_item_price=$8 
     local next_item_profit=$9 
-    local next_item_cooldown=$10
+    local next_item_cooldown=${10}
 
-    # اگر کارت اول در کولدان است، کارت دوم را بررسی کن
-    if [[ -n "$cooldown" && "$cooldown" -gt 0 ]]; then
+    # Calculate the ratios
+    left_side=$(echo "($threshold) * ($price/$profit)" | bc -l)
+    right_side=$(echo "$next_item_price/$next_item_profit" | bc -l)
+
+    if [[ -z "$cooldown" || "$cooldown" -eq 0 ]]; then
+        echo "$best_item_id"
+    elif [[ -v next_item_cooldown && -n "$next_item_cooldown" && "$next_item_cooldown" -ne 0 ]]; then
+        echo "$best_item_id"
+    elif (( $(echo "$left_side > $right_side" | bc -l) )); then
         echo "$next_item_id"
     else
         echo "$best_item_id"
@@ -153,7 +146,6 @@ choose_card_to_upgrade() {
 # Main script logic
 main() {
     while true; do
-        # Get the best item to buy
         best_item=$(get_best_item)
         best_item_id=$(echo "$best_item" | jq -r '.id')
         section=$(echo "$best_item" | jq -r '.section')
@@ -162,7 +154,6 @@ main() {
         cooldown=$(echo "$best_item" | jq -r '.cooldownSeconds')
         echo -e "${blue}The best item to buy:${yellow} $best_item_id${rest}"
 
-        # Get the second item
         second_item=$(get_second_best_item)
         next_item_id=$(echo "$second_item" | jq -r '.id')
         next_item_section=$(echo "$second_item" | jq -r '.section')
@@ -171,60 +162,50 @@ main() {
         next_item_cooldown=$(echo "$second_item" | jq -r '.cooldownSeconds')
         echo -e "${blue}The Second best item to buy:${yellow} $next_item_id${rest}"
 
-        # Select the best card based on cooldown
+        # انتخاب بهترین کارت بر اساس کول‌دان
         selected_item_id=$(choose_card_to_upgrade "$best_item_id" "$section" "$price" "$profit" "$cooldown" "$next_item_id" "$next_item_section" "$next_item_price" "$next_item_profit" "$next_item_cooldown")
-        echo -e "${blue}Best item to buy:${yellow} $selected_item_id in section: $section"
-        echo -e "${blue}Price:${yellow} $price ${blue}Profit per hour:${yellow} $profit ${blue}Cooldown seconds:${yellow} $cooldown${rest}"
-        echo -e "${purple}============================${rest}"
+        echo -e "${blue}Best card selected with id:${yellow} $selected_item_id${rest}"
 
-        # Check balance
-        balance_response=$(curl -s -X POST -H "Content-Type: application/json" \
+        current_balance=$(curl -s -X POST -H "User-Agent: Mozilla/5.0 (Android 12; Mobile; rv:102.0) Gecko/102.0 Firefox/102.0" \
+            -H "Accept: */*" \
+            -H "Accept-Language: en-US,en;q=0.5" \
+            -H "Referer: https://hamsterkombat.io/" \
             -H "Authorization: $Authorization" \
             -H "Origin: https://hamsterkombat.io" \
-            -H "Referer: https://hamsterkombat.io/" \
-            https://api.hamsterkombatgame.io/clicker/user)
+            -H "Connection: keep-alive" \
+            -H "Sec-Fetch-Dest: empty" \
+            -H "Sec-Fetch-Mode: cors" \
+            -H "Sec-Fetch-Site: same-site" \
+            -H "Priority: u=4" \
+            https://api.hamsterkombatgame.io/clicker/get-profile | jq -r '.hamster.token')
 
-        echo -e "${yellow}Balance response:${rest} $balance_response"
-
-        # بررسی خطای 'NOT_FOUND'
-        if [[ "$balance_response" == "NOT_FOUND" ]]; then
-            echo -e "${red}Authorization یا API endpoint نامعتبر است. لطفاً بررسی کنید و دوباره امتحان کنید.${rest}"
-            exit 1
-        fi
-
-        balance=$(echo "$balance_response" | jq -r '.user.balance')
-
-        echo -e "${green}Balance:${yellow} $balance${rest}"
-        if (( $(echo "$balance < $min_balance_threshold" | bc -l) )); then
-            echo -e "${red}Balance is below the threshold. Stopping the script.${rest}"
+        if (( $(echo "$current_balance < $price" | bc -l) )); then
+            echo -e "${red}Not enough balance to purchase the card. Stopping script.${rest}"
             break
         fi
 
-        # Wait if cooldown is active
-        if [ "$cooldown" -gt 0 ]; then
-            wait_for_cooldown "$cooldown"
+        if (( $(echo "$current_balance < $min_balance_threshold" | bc -l) )); then
+            echo -e "${red}Current balance is below the minimum balance threshold. Stopping script.${rest}"
+            break
         fi
 
-        # Purchase upgrade
-        if (( $(echo "$balance >= $price" | bc -l) )); then
-            response=$(purchase_upgrade "$best_item_id")
-            if [[ "$response" == *"Success"* ]]; then
-                echo -e "${green}Successfully purchased upgrade:${yellow} $best_item_id${rest}"
-                total_spent=$(echo "$total_spent + $price" | bc)
-                total_profit=$(echo "$total_profit + $profit" | bc)
-                echo -e "${cyan}Total spent so far:${yellow} $total_spent${rest}"
-                echo -e "${cyan}Total profit so far:${yellow} $total_profit${rest}"
-            else
-                echo -e "${red}Failed to purchase upgrade. Response:${yellow} $response${rest}"
+        if [[ "$selected_item_id" == "$best_item_id" ]]; then
+            # اگر کارت انتخاب شده همان بهترین کارت است اما در کول‌دان باشد
+            if [[ "$cooldown" -ne 0 ]]; then
+                wait_for_cooldown "$cooldown"
             fi
-        else
-            echo -e "${red}Insufficient balance to purchase upgrade.${rest}"
+        elif [[ "$selected_item_id" == "$next_item_id" && "$next_item_cooldown" -ne 0 ]]; then
+            # اگر کارت انتخاب شده دومین کارت بهترین است اما در کول‌دان باشد
+            wait_for_cooldown "$next_item_cooldown"
         fi
 
-        # Wait before next check
+        response=$(purchase_upgrade "$selected_item_id")
+        echo -e "${green}Purchase response:${rest} $response"
+        echo ""
+
+        echo -e "${yellow}Waiting 10 seconds before the next check...${rest}"
         sleep 10
     done
 }
 
-# Run the main logic
 main
