7e65dc60ff11646303e830955a1a35846d61b3f9
fulken <32429584+fulken@users.noreply.github.com>
2024-08-16 00:35:25 +0330

    Update ham_keys.sh

--- ham_keys.sh
+++ ham_keys.sh
@@ -45,21 +45,6 @@ PROXY_FILE="proxy.txt"
 
 # Games
 declare -A games
-games[1, name]="Riding Extreme 3D"
-games[1, appToken]="d28721be-fd2d-4b45-869e-9f253b554e50"
-games[1, promoId]="43e35910-c168-4634-ad4f-52fd764a843f"
-
-games[2, name]="Chain Cube 2048"
-games[2, appToken]="d1690a07-3780-4068-810f-9b5bbf2931b2"
-games[2, promoId]="b4170868-cef0-424f-8eb9-be0622e8e8e3"
-
-games[3, name]="My Clone Army"
-games[3, appToken]="74ee0b5b-775e-4bee-974f-63e7f4d5bacb"
-games[3, promoId]="fe693b26-b342-4159-8808-15e3ff7f8767"
-
-games[4, name]="Train Miner"
-games[4, appToken]="82647f43-3f87-402d-88dd-09a90025313f"
-games[4, promoId]="c4480ac7-e178-4973-8061-9ed5b2e17954"
 
 games[5, name]="Merge Away"
 games[5, appToken]="8d1cc2ad-e097-4b86-90ef-7a27e19fb833"
