05ae1169ef62914ffc8ceb56d93e6d0b03690374
fulken <32429584+fulken@users.noreply.github.com>
2024-08-17 03:39:37 +0330

    Update ham_keys_2.sh

--- ham_keys_2.sh
+++ ham_keys_2.sh
@@ -1,99 +1,42 @@
-#!/bin/bash
-
-# Colors
+#!/bin/bash# Colors
 yellow='\033[0;33m'
 purple='\033[0;35m'
 green='\033[0;32m'
-rest='\033[0m'
-
-if ! command -v jq &> /dev/null
-then
-    # Check if the environment is Termux
-    if [ -n "$TERMUX_VERSION" ]; then
+rest='\033[0m'if ! command -v jq &> /dev/null
+then# Check if the environment is Termuxif [ -n "$TERMUX_VERSION" ]; then
         pkg update -y
         pkg install -y jq
     else
         apt update -y && apt install -y jq
-    fi
-fi
-
-if ! command -v uuidgen &> /dev/null
-then
-    # Check if the environment is Termux
-    if [ -n "$TERMUX_VERSION" ]; then
+    fifiif ! command -v uuidgen &> /dev/null
+then# Check if the environment is Termuxif [ -n "$TERMUX_VERSION" ]; then
         pkg install uuid-utils -y
     else
         apt update -y && apt install uuid-runtime -y
-    fi
-fi
+    fifi
 
 clear
-echo -e "${purple}=======${yellow}Hamster Combat Game Keys${purple}=======${rest}"
-echo ""
-echo -en "${purple}[Optional] ${green}Enter Your telegram Bot token: ${rest}"
-read -r TELEGRAM_BOT_TOKEN
-echo -e "${purple}============================${rest}"
-echo -en "${purple}[Optional] ${green}Enter Your Telegram Channel ID [example: ${yellow}@P_Tech2024${green}]: ${rest}"
-read -r TELEGRAM_CHANNEL_ID
-echo -e "${purple}============================${rest}"
-echo -e "${green}generating ... Keys will be saved in [${yellow}my_keys.txt${green}]..${rest}"
+echo -e "${purple}=======${yellow}Hamster Combat Game Keys${purple}=======${rest}"echo""echo -en "${purple}[Optional] ${green}Enter Your telegram Bot token: ${rest}"read -r TELEGRAM_BOT_TOKEN
+echo -e "${purple}============================${rest}"echo -en "${purple}[Optional] ${green}Enter Your Telegram Channel ID [example: ${yellow}@P_Tech2024${green}]: ${rest}"read -r TELEGRAM_CHANNEL_ID
+echo -e "${purple}============================${rest}"echo -e "${green}generating ... Keys will be saved in [${yellow}my_keys.txt${green}]..${rest}"
 
 EVENTS_DELAY=20
-PROXY_FILE="proxy.txt"
-# Set bot as channel admin. and enable manage message in admin settings.
-# ربات را به عنوان ادمین کانال انتخاب کنید و manage message را فعال کنید.
-
-# Games
-declare -A games
-games[1, name]="Riding Extreme 3D"
-games[1, appToken]="d28721be-fd2d-4b45-869e-9f253b554e50"
-games[1, promoId]="43e35910-c168-4634-ad4f-52fd764a843f"
-
-games[2, name]="Chain Cube 2048"
-games[2, appToken]="d1690a07-3780-4068-810f-9b5bbf2931b2"
-games[2, promoId]="b4170868-cef0-424f-8eb9-be0622e8e8e3"
-
-games[3, name]="My Clone Army"
-games[3, appToken]="74ee0b5b-775e-4bee-974f-63e7f4d5bacb"
-games[3, promoId]="fe693b26-b342-4159-8808-15e3ff7f8767"
-
-games[4, name]="Train Miner"
-games[4, appToken]="82647f43-3f87-402d-88dd-09a90025313f"
-games[4, promoId]="c4480ac7-e178-4973-8061-9ed5b2e17954"
-
-games[5, name]="Merge Away"
-games[5, appToken]="8d1cc2ad-e097-4b86-90ef-7a27e19fb833"
-games[5, promoId]="dc128d28-c45b-411c-98ff-ac7726fbaea4"
-
-games[6, name]="Twerk Race 3D"
-games[6, appToken]="61308365-9d16-4040-8bb0-2f4a4c69074c"
-games[6, promoId]="61308365-9d16-4040-8bb0-2f4a4c69074c"
-
-# Proxys
-load_proxies() {
-	if [[ -f "$1" ]]; then
-		mapfile -t proxies <"$1"
-	else
-		echo -e "${yellow}Proxy file not found. We continue without a proxy.${rest}"
+PROXY_FILE="proxy.txt"# Only keep the "Twerk Race 3D" gamedeclare -A games
+games[1, name]="Twerk Race 3D"
+games[1, appToken]="61308365-9d16-4040-8bb0-2f4a4c69074c"
+games[1, promoId]="61308365-9d16-4040-8bb0-2f4a4c69074c"# Proxiesload_proxies() {
+	if [[ -f "$1" ]]; thenmapfile -t proxies <"$1"elseecho -e "${yellow}Proxy file not found. We continue without a proxy.${rest}"
 		proxies=()
 	fi
 }
 
-# client_id
-generate_client_id() {
-	echo "$(date +%s%3N)-$(cat /dev/urandom | tr -dc '0-9' | fold -w 19 | head -n 1)"
+# client_idgenerate_client_id() {
+	echo"$(date +%s%3N)-$(cat /dev/urandom | tr -dc '0-9' | fold -w 19 | head -n 1)"
 }
 
-#login
-login() {
-	local client_id=$1
-	local app_token=$2
-	local proxy=${3:-}
-
-	local proxy_option=""
-	if [[ -n "$proxy" ]]; then
-		proxy_option="--proxy $proxy"
-	fi
+#loginlogin() {
+	local client_id=$1local app_token=$2local proxy=${3:-}local proxy_option=""if [[ -n "$proxy" ]]; then
+		proxy_option="--proxy $proxy"fi
 
 	response=$(
 		curl -s $proxy_option -X POST -H "Content-Type: application/json" \
@@ -101,23 +44,12 @@ login() {
 		"https://api.gamepromo.io/promo/login-client"
 	)
 
-	if [[ $? -ne 0 ]]; then
-		return
-	fi
-
-	echo "$response" | jq -r '.clientToken'
+	if [[ $? -ne 0 ]]; thenreturnfiecho"$response" | jq -r '.clientToken'
 }
 
-# Progress
-emulate_progress() {
-	local client_token=$1
-	local promo_id=$2
-	local proxy=${3:-}
-
-	local proxy_option=""
-	if [[ -n "$proxy" ]]; then
-		proxy_option="--proxy $proxy"
-	fi
+# Progressemulate_progress() {
+	local client_token=$1local promo_id=$2local proxy=${3:-}local proxy_option=""if [[ -n "$proxy" ]]; then
+		proxy_option="--proxy $proxy"fi
 
 	response=$(
 		curl -s $proxy_option -X POST -H "Authorization: Bearer $client_token" \
@@ -126,24 +58,12 @@ emulate_progress() {
 		"https://api.gamepromo.io/promo/register-event"
 	)
 
-	if [[ $? -ne 0 ]]; then
-		echo "Error during emulate progress"
-		return
-	fi
-
-	echo "$response" | jq -r '.hasCode'
+	if [[ $? -ne 0 ]]; thenecho"Error during emulate progress"returnfiecho"$response" | jq -r '.hasCode'
 }
 
-# Promotion keys
-generate_key() {
-	local client_token=$1
-	local promo_id=$2
-	local proxy=${3:-}
-
-	local proxy_option=""
-	if [[ -n "$proxy" ]]; then
-		proxy_option="--proxy $proxy"
-	fi
+# Promotion keysgenerate_key() {
+	local client_token=$1local promo_id=$2local proxy=${3:-}local proxy_option=""if [[ -n "$proxy" ]]; then
+		proxy_option="--proxy $proxy"fi
 
 	response=$(
 		curl -s $proxy_option -X POST -H "Authorization: Bearer $client_token" \
@@ -152,16 +72,10 @@ generate_key() {
 		"https://api.gamepromo.io/promo/create-code"
 	)
 
-	if [[ $? -ne 0 ]]; then
-		echo "Error during generate key"
-		return
-	fi
-
-	echo "$response" | jq -r '.promoCode'
+	if [[ $? -ne 0 ]]; thenecho"Error during generate key"returnfiecho"$response" | jq -r '.promoCode'
 }
 
-# Send to telegram
-send_to_telegram() {
+# Send to telegramsend_to_telegram() {
     local message=$1
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
         -d chat_id="$TELEGRAM_CHANNEL_ID" \
@@ -169,58 +83,32 @@ send_to_telegram() {
         -d parse_mode="MarkdownV2" > /dev/null 2>&1
 }
 
-# key process
-generate_key_process() {
-	local app_token=$1
-	local promo_id=$2
-	local proxy=$3
+# key processgenerate_key_process() {
+	local app_token=$1local promo_id=$2local proxy=$3
 
 	client_id=$(generate_client_id)
-	client_token=$(login "$client_id" "$app_token" "$proxy")
+	client_token=$(login "$client_id""$app_token""$proxy")
 
-	if [[ -z "$client_token" ]]; then
-		return
-	fi
+	if [[ -z "$client_token" ]]; thenreturnfifor i in {1..20}; dosleep $((EVENTS_DELAY * (RANDOM % 4 + 1) / 3))
+		has_code=$(emulate_progress "$client_token""$promo_id""$proxy")
 
-	for i in {1..20}; do
-		sleep $((EVENTS_DELAY * (RANDOM % 4 + 1) / 3))
-		has_code=$(emulate_progress "$client_token" "$promo_id" "$proxy")
+		if [[ "$has_code" == "true" ]]; thenbreakfidone
 
-		if [[ "$has_code" == "true" ]]; then
-			break
-		fi
-	done
-
-	key=$(generate_key "$client_token" "$promo_id" "$proxy")
-	echo "$key"
+	key=$(generate_key "$client_token""$promo_id""$proxy")
+	echo"$key"
 }
 
-# main
-main() {
-	load_proxies "$PROXY_FILE"
+# mainmain() {
+	load_proxies "$PROXY_FILE"whiletrue; doif [[ ${#proxies[@]} -gt 0 ]]; then
+			proxy=${proxies[RANDOM % ${#proxies[@]}]}else
+			proxy=""fi
 
-	while true; do
-		for game_choice in {1..6}; do
-			if [[ ${#proxies[@]} -gt 0 ]]; then
-				proxy=${proxies[RANDOM % ${#proxies[@]}]}
-			else
-				proxy=""
-			fi
+		key=$(generate_key_process "${games[1, appToken]}""${games[1, promoId]}""$proxy")
 
-			key=$(generate_key_process "${games[$game_choice, appToken]}" "${games[$game_choice, promoId]}" "$proxy")
-
-			if [[ -n "$key" ]]; then
-				message="${games[$game_choice, name]} : $key"
-				telegram_message="\`${key}\`"
-				echo "$message" | tee -a my_keys.txt
-				send_to_telegram "$telegram_message"
-			else
-				echo "Error generating key for ${games[$game_choice, name]}"
-			fi
-
-			sleep 10 # wait
-		done
-	done
+		if [[ -n "$key" ]]; then
+			message="${games[1, name]} : $key"
+			telegram_message="\`${key}\`"echo"$message" | tee -a my_keys.txt
+			send_to_telegram "$telegram_message"elseecho"Error generating key for ${games[1, name]}"fisleep 10 # waitdone
 }
 
 main
